036cfb8d943f9f007398fd42814ebb00c6dd7fb4
ayesharafia <ayeraf.99@gmail.com>
2026-04-28 16:19:27 +0530

    10th commit

--- DressifyMe/src/app/app.routes.ts
+++ DressifyMe/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Studio } from './studio/studio'
 import { Bag } from './bag/bag';
+import { ContactUs } from './contact-us/contact-us';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'studio', component: Studio },
   { path: 'bag', component: Bag },
   { path: '', component: Home },
-
+  { path:'contact',component:ContactUs}
 ];
